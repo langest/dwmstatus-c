cc83a0ca8f194abb63e87ab976d46425a8e5aaa9
Daniel Langesten <daniel.langest@gmail.com>
2017-07-31 19:37:42 +0200

    Adding debugging macros

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 dwmStatus: dwmstatus.c
 	gcc -o dwmstatus dwmstatus.c -Wall -std=c99 -lX11
 
+debug:
+	gcc -o dwmstatus dwmstatus.c -DDEBUG -Wall -std=c99 -lX11
+
 clean:
 	rm dwmstatus
--- dwmstatus.c
+++ dwmstatus.c
@@ -1,5 +1,13 @@
 #include <X11/Xlib.h>
 
+#if defined(DEBUG) && DEBUG > 0
+#define DEBUG_PRINT(fmt, ...) fprintf(stderr, "[DEBUG] %s:%d:%s(): " fmt, __FILE__, __LINE__, __func__, __VA_ARGS__)
+#define DEBUG_ERROR(fmt) fprintf(stderr, "[ERROR] %s:%d:%s(): " fmt, __FILE__, __LINE__, __func__)
+#else
+#define DEBUG_PRINT(fmt, ...)
+#define DEBUG_ERROR(fmt)
+#endif
+
 int main() {
 	Display* display;
 	display = XOpenDisplay(NULL);
