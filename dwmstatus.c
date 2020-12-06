6bb10721c4b3d59f030d27880fae0ca97cf37733
Daniel Langesten <daniel.langest@gmail.com>
2020-12-06 16:29:34 +0100

    Ping to check if VPN is up

--- dwmstatus.c
+++ dwmstatus.c
@@ -1,9 +1,10 @@
 #define _DEFAULT_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
 #include <signal.h>
-#include <sys/stat.h>
+#include <sys/wait.h>
 
 #include <alsa/asoundlib.h>
 
@@ -18,53 +19,64 @@
 #define DEBUG_ERROR(fmt)
 #endif
 
-void GetVpnStatus(char* out, size_t size) {
-	struct stat s;
-	const char* vpnPath = "/sys/class/net/tun0";
-	int err = lstat(vpnPath, &s);
-	if (err == -1) {
-		if(errno == ENOENT) {
-			DEBUG_PRINT("VPN tunnel not found: %s\n", vpnPath);
-		} else {
-			DEBUG_ERROR("Error when checking VPN tunnel status\n");
-		}
-		snprintf(out, size, "no");
-		return;
+/* Blocking ping */
+/* Blocking shouldn't be a problem
+ * since the rest of the program is
+ * waiting for almost a minute. So
+ * there is plenty of time to await
+ * a response
+ */
+int try_ping(const char* ip_address, const char* timeout) {
+	if(fork() == 0) { //child
+		execl("/bin/ping", "ping", "-w", timeout, "-c1", ip_address, "&>", "/dev/null", (char*)NULL);
 	}
-	DEBUG_PRINT("VPN tunnel exists: %s\n", vpnPath);
-	snprintf(out, size, "yes");
+	int status;
+	wait(&status);
+	return status == 0;
 }
 
-int ReadFile(char* fileName, float* out) {
+	const char* ip = "xxx.xxx.xxx.xxx"; // Any ip that you can only reach when VPN is up
+void get_vpn_status(char* out, size_t size, const char* timeout) {
+
+	if (try_ping(ip, timeout) == 1) {
+		DEBUG_ERROR("Could reach ip, VPN appears to be up\n");
+		snprintf(out, size, "yes");
+		return;
+	}
+	DEBUG_ERROR("Could NOT reach ip, VPN appears to be down\n");
+	snprintf(out, size, "no");
+}
+
+int read_file(char* file, float* out) {
 	FILE* file;
-	file = fopen(fileName, "r");
+	file = fopen(file, "r");
 	if (file == NULL) {
-		DEBUG_PRINT("Failed to open file: %s\n", fileName);
+		DEBUG_PRINT("Failed to open file: %s\n", file);
 		return 1;
 	}
 	int err = fscanf(file, "%f", out);
 	if (err < 0) {
-		DEBUG_PRINT("Failed to scan file: %s\n", fileName);
+		DEBUG_PRINT("Failed to scan file: %s\n", file);
 		return 2;
 	}
 	err = fclose(file);
 	if (err != 0) {
-		DEBUG_PRINT("Failed to close file: %s\n", fileName);
+		DEBUG_PRINT("Failed to close file: %s\n", file);
 		return 2;
 	}
 	return 0;
 }
 
-void GetScreenBrightness(char* out, const size_t size) {
+void get_screen_brightness(char* out, const size_t size) {
 	float max;
-	int err = ReadFile("/sys/class/backlight/intel_backlight/max_brightness", &max);
+	int err = read_file("/sys/class/backlight/intel_backlight/max_brightness", &max);
 	if (err != 0){
 		snprintf(out, size, "err");
 		return;
 	}
 
 	float now;
-	err = ReadFile("/sys/class/backlight/intel_backlight/actual_brightness", &now);
+	err = read_file("/sys/class/backlight/intel_backlight/actual_brightness", &now);
 	if (err != 0) {
 		snprintf(out, size, "err");
 		return;
@@ -74,12 +86,12 @@ void GetScreenBrightness(char* out, const size_t size) {
 	snprintf(out, size, "%0.f%%", now/max*100.0f);
 }
 
-void GetBatteryStatus(int batIndex, char* out, const size_t size) {
+void get_battery_status(int battery_index, char* out, const size_t size) {
 	int capacity;
 	FILE* file;
-	int filenameMaxLength = 40;
-	char info_file[filenameMaxLength];
-	snprintf(info_file, filenameMaxLength, "/sys/class/power_supply/BAT%d/capacity", batIndex);
+	int path_max_length = 40;
+	char info_file[path_max_length];
+	snprintf(info_file, path_max_length, "/sys/class/power_supply/BAT%d/capacity", battery_index);
 
 	file = fopen(info_file, "r");
 	if (file) {
@@ -107,9 +119,9 @@ void GetBatteryStatus(int batIndex, char* out, const size_t size) {
 	}
 
 	/* Get battery status */
-	const size_t statusSize = 16;
-	char status[statusSize];
-	snprintf(info_file, filenameMaxLength, "/sys/class/power_supply/BAT%d/status", batIndex);
+	const size_t status_size = 16;
+	char status[status_size];
+	snprintf(info_file, path_max_length, "/sys/class/power_supply/BAT%d/status", battery_index);
 	file = fopen(info_file, "r");
 	if (file == NULL) {
 		DEBUG_ERROR("Failed to open battery status file\n");
@@ -126,9 +138,9 @@ void GetBatteryStatus(int batIndex, char* out, const size_t size) {
 			DEBUG_ERROR("Failed to close battery status file\n");
 		}
 		if (status[0] == 'C') { /* Charging */
-			snprintf(status, statusSize, "%s", "⌁⏶");
+			snprintf(status, status_size, "%s", "⌁⏶");
 		} else if (status[0] == 'D'){ /* Discharging */
-			snprintf(status, statusSize, "%s", "⌁⏷");
+			snprintf(status, status_size, "%s", "⌁⏷");
 		} else { /* Uknown / not used */
 			status[0] = '\0';
 		}
@@ -137,7 +149,7 @@ void GetBatteryStatus(int batIndex, char* out, const size_t size) {
 	snprintf(out, size, "%s%d%%", status, capacity);
 }
 
-void GetAudioVolume(long* outvol) {
+void get_audio_volume(long* outvol) {
 	*outvol = -1;
 	snd_mixer_t* handle;
 	snd_mixer_elem_t* elem;
@@ -199,52 +211,52 @@ void GetAudioVolume(long* outvol) {
 	snd_mixer_close(handle);
 }
 
-int GetTime(char* out, const size_t size) {
+int get_time(char* out, const size_t size) {
 	time_t t;
-	struct tm* localTime;
+	struct tm* local_time;
 
 	time(&t);
-	localTime = localtime(&t);
-	if (localTime == NULL) {
-		DEBUG_ERROR("Failed to get localTime\n");
+	local_time = localtime(&t);
+	if (local_time == NULL) {
+		DEBUG_ERROR("Failed to get local_time\n");
 		return 60;
 	}
 
 	char time[size];
-	int len = strftime(time, size, "W%W %%s %d %b %H:%M", localTime);
+	int len = strftime(time, size, "W%W %%s %d %b %H:%M", local_time);
 	if (len <= 0) {
 		DEBUG_ERROR("Failed to format time\n");
 	}
 	static const char* weekdays = "日\0月\0火\0水\0木\0金\0土\0";
-	snprintf(out, size, time, &weekdays[localTime->tm_wday*4]);
-	return localTime->tm_sec;
+	snprintf(out, size, time, &weekdays[local_time->tm_wday*4]);
+	return local_time->tm_sec;
 }
 
-void GetKeyboardLayout(Display* display, char* out, const size_t size) {
+void get_keyboard_layout(Display* display, char* out, const size_t size) {
 	XkbDescRec* _kbdDescPtr = XkbAllocKeyboard();
 	XkbGetNames(display, XkbSymbolsNameMask, _kbdDescPtr);
-	Atom symName = _kbdDescPtr->names->symbols;
-	char* layoutString = XGetAtomName(display, symName);
+	Atom sym_name = _kbdDescPtr->names->symbols;
+	char* layout_string = XGetAtomName(display, sym_name);
 
 	/* Currently configured for US and SE */
-	if (strstr(layoutString, "us")) {
+	if (strstr(layout_string, "us")) {
 		snprintf(out, size, "us");
-	} else if (strstr(layoutString, "se")) {
+	} else if (strstr(layout_string, "se")) {
 		snprintf(out, size, "se");
 	}
 	XkbFreeKeyboard(_kbdDescPtr, XkbSymbolsNameMask, True);
 }
 
-void SetStatus(char* status, Display* display) {
+void set_status(char* status, Display* display) {
 	XStoreName(display, DefaultRootWindow(display), status);
 	XSync(display, False);
 }
 
-void CatchSignal(int signo, siginfo_t* sinfo, void *context) {
+void catch_signal(int signo, siginfo_t* sinfo, void *context) {
 	DEBUG_PRINT("Caught signal no: %d.\n", signo);
 }
 
-void CatchSignalAndSleep(int signo, siginfo_t* sinfo, void *context) {
+void catch_signal_and_sleep(int signo, siginfo_t* sinfo, void *context) {
 	DEBUG_PRINT("Caught signal no: %d.\n", signo);
 	nanosleep((const struct timespec[]){{0, 160000000L}}, NULL);
 }
@@ -253,13 +265,13 @@ int main() {
 	/* Register signal handler */
 	struct sigaction action;
 	memset(&action, 0, sizeof(action));
-	action.sa_sigaction = &CatchSignal;
+	action.sa_sigaction = &catch_signal;
 	action.sa_flags = SA_SIGINFO;
 	if (sigaction(SIGUSR1, &action, NULL) < 0) {
 		DEBUG_ERROR("Failed to register signal handler\n");
 		return 2;
 	}
-	action.sa_sigaction = &CatchSignalAndSleep;
+	action.sa_sigaction = &catch_signal_and_sleep;
 	if (sigaction(SIGUSR2, &action, NULL) < 0) {
 		DEBUG_ERROR("Failed to register signal handler\n");
 		return 2;
@@ -272,33 +284,34 @@ int main() {
 		return 1;
 	}
 
-	const size_t brightnessMaxLength = 8;
-	const size_t batteryMaxLength = 16;
-	const size_t kbMaxLength = 4;
-	const size_t vpnMaxLength = 8;
-	const size_t timeMaxLength = 32;
-	const size_t statusMaxLength = 128;
-	char brightness[brightnessMaxLength];
-	char battery0[batteryMaxLength];
-	char battery1[batteryMaxLength];
-	char kb[kbMaxLength];
+	const size_t brightness_max_length = 8;
+	const size_t battery_max_length = 16;
+	const size_t keyboard_max_length = 4;
+	const size_t vpn_max_length = 8;
+	const size_t time_max_length = 32;
+	const size_t status_max_length = 128;
+	const char* vpn_timeout = "10";
+	char brightness[brightness_max_length];
+	char battery0[battery_max_length];
+	char battery1[battery_max_length];
+	char kb[keyboard_max_length];
 	long volume = -1;
-	char vpn[vpnMaxLength];
-	char time[timeMaxLength];
-	char status[statusMaxLength];
+	char vpn[vpn_max_length];
+	char time[time_max_length];
+	char status[status_max_length];
 
-	int sleepDuration;
+	int sleep_duration;
 	while (True) {
-		GetScreenBrightness(brightness, brightnessMaxLength);
-		GetBatteryStatus(0, battery0, batteryMaxLength);
-		GetBatteryStatus(1, battery1, batteryMaxLength);
-		GetKeyboardLayout(display, kb, kbMaxLength);
-		GetAudioVolume(&volume);
-		GetVpnStatus(vpn, vpnMaxLength);
+		get_screen_brightness(brightness, brightness_max_length);
+		get_battery_status(0, battery0, battery_max_length);
+		get_battery_status(1, battery1, battery_max_length);
+		get_keyboard_layout(display, kb, keyboard_max_length);
+		get_audio_volume(&volume);
+		get_vpn_status(vpn, vpn_max_length, vpn_timeout);
 
-		sleepDuration = 60 - GetTime(time, timeMaxLength);
+		sleep_duration = 60 - get_time(time, time_max_length);
 
-		snprintf(status, statusMaxLength, " bklight: %s | bat: %s, %s | ⌨ %s | 🔊 %ld%% | vpn %s | %s",
+		snprintf(status, status_max_length, " bklight: %s | bat: %s, %s | ⌨ %s | 🔊 %ld%% | vpn %s | %s",
 		brightness,
 		battery0,
 		battery1,
@@ -307,9 +320,9 @@ int main() {
 		vpn,
 		time);
 
-		SetStatus(status, display);
-		DEBUG_PRINT("Set status to: %s, sleeping for %d seconds\n", status, sleepDuration);
-		sleep(sleepDuration);
+		set_status(status, display);
+		DEBUG_PRINT("Set status to: %s, sleeping for %d seconds\n", status, sleep_duration);
+		sleep(sleep_duration);
 	}
 
 	XCloseDisplay(display);
